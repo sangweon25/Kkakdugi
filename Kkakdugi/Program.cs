9c79babaf0e3965126854937fd441eb2a1aaccc6
peachhike <peachhike@naver.com>
2025-02-07 00:32:19 +0900

    게임 시작 화면

--- Kkakdugi/Program.cs
+++ Kkakdugi/Program.cs
@@ -6,8 +6,25 @@
         {
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-            Console.WriteLine("1./2.");
+            
+            // 입력
             Console.Write("원하시는 행동을 입력해주세요.");
+            string userInput = Console.ReadLine();
+            
+            // 출력.예외 포함..if
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("1. 상태 보기");
+                    break;
+                case "2":
+                    Console.WriteLine("2. 전투 시작");
+                    break;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    break;
+            }
+            
         }
     }
 }
