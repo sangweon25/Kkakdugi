ac556c0c7ca52a2d55b11d47b50fd1feca90aef4
pxo2 <rlarbxodia@gmail.com>
2025-02-07 16:27:41 +0900

    tae_Result_4
    수정

--- Kkakdugi/Result.cs
+++ Kkakdugi/Result.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace Kkakdugi
 {
-    internal class Result
+   
+    internal class Program
     {
+        public void Result(string[] args)
+        {
+            int playerHP = 100;
+            int monster = 3;
+            Random random = new Random();
+
+            while (playerHP > 0 && monster > 0)
+            {
+                int damage = random.Next(10);
+                playerHP -= damage;
+                monster--;
+
+                Console.WriteLine("Battle!! - Result");
+
+                if (playerHP > 0 && monster == 0)
+                {
+                    Console.WriteLine("Victory");
+                    Console.WriteLine("던전에서 몬스터 3마리를 잡았습니다.");
+                    Console.WriteLine($"Lv.1 Chad\nHp -> {playerHP}");
+                    break;
+                }
+                else if (monster > 0 && playerHP <= 0)
+                {
+                    Console.WriteLine("You Lose");
+                    Console.WriteLine($"Lv.1 Chad\nHp -> {playerHP}");
+                    break;
+                }
+
+            }
+            Console.WriteLine("0. 다음");
+            Console.Write(">>");
+            Console.ReadLine();
+            int intput = ConsoleUtility.GetInput(0, 0);
+
+            if (intput == 0)
+            {
+                //Main();
+            }
+        }
+
+    }
+    static public class ConsoleUtility
+    {
+        static public int GetInput(int min, int max)
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                if (int.TryParse(Console.ReadLine(), out int input) && (input >= min) && (input <= max))
+                    return input;
+
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해 주세요");
+            }
+        }
     }
 }
