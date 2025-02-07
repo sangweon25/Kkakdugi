2d10032a6b4e54762a874e2c19b7981f2acd2c7c
pxo2 <rlarbxodia@gmail.com>
2025-02-07 10:08:25 +0900

    tae_Result_조건문
    조건문 진행중1

--- Kkakdugi/Program.cs
+++ Kkakdugi/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
+            int playerHP = 100;
+            int monster = 3;
+            Random random = new Random();
 
+            while (playerHP > 0 && monster > 0)
+            {
+                int damage = random.Next(10);
+                playerHP -= damage;
+                monster--;
+
+                Console.WriteLine("Battle!! - Result");
+
+                if(playerHP > 0 && monster == 0)
+                {
+                    Console.WriteLine("Victory");
+                    break;
+                }
+                else if (monster > 0 && playerHP == 0)
+                {
+                    Console.WriteLine("You Lose");
+                    break;
+                }
+
+            }
         }
     }
 }
