9a62caa7e571ff67eb7c0a2ff3f354f1d77f49a1
pxo2 <rlarbxodia@gmail.com>
2025-02-07 10:57:30 +0900

    tae_Result_조건문_1
    조건문 추가

--- Kkakdugi/Program.cs
+++ Kkakdugi/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Kkakdugi
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kkakdugi
 {
     internal class Program
     {
@@ -19,6 +25,8 @@
                 if(playerHP > 0 && monster == 0)
                 {
                     Console.WriteLine("Victory");
+                    Console.WriteLine("던전에서 몬스터 3마리를 잡았습니다.");
+                    Console.WriteLine($"Lv.1 Chad\nplayerHp -> {playerHP}");
                     break;
                 }
                 else if (monster > 0 && playerHP == 0)
@@ -28,6 +36,28 @@
                 }
 
             }
+            Console.WriteLine("0. 다음");
+            Console.Write(">>");
+            Console.ReadLine();
+            int intput = ConsoleUtility.GetInput(0, 0);
+
+        }
+
+    }
+
+    static public class ConsoleUtility
+    {
+        static public int GetInput(int min, int max)
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                if (int.TryParse(Console.ReadLine(), out int input) && (input >= min) && (input <= max))
+                    return input;
+
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해 주세요");
+            }
+            
         }
     }
 }
