eb5b25e7655900a8fe55e54eee78893cb6369ad5
peachhike <peachhike@naver.com>
2025-02-07 15:07:42 +0900

    프로그램2삭제

--- Kkakdugi/Program.cs
+++ Kkakdugi/Program.cs
@@ -6,12 +6,9 @@
         {
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-            
-            // 입력
-            Console.Write("원하시는 행동을 입력해주세요.");
-            string userInput = Console.ReadLine();
-            
+
             // 출력.예외 포함..if
+            string userInput = "1";
             switch (userInput)
             {
                 case "1":
@@ -24,7 +21,31 @@
                     Console.WriteLine("잘못된 입력입니다.");
                     break;
             }
-            
+
+            Console.WriteLine("상태 보기");
+            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+
+//캐릭터 객체 생성 및 초기화
+
+            bool gameRunning = true;
+
+            while (gameRunning)
+            {
+                // 캐릭터 상태 보기 출력
+                Console.Write("원하시는 행동을 입력해주세요.");
+                string str = Console.ReadLine();
+
+
+                switch (str)
+                {
+                    case "1":
+                        Console.WriteLine("1");
+                        break;
+                    case "0":
+                        Console.WriteLine("0. 나가기");
+                        break;
+                }
+            }
         }
     }
 }
