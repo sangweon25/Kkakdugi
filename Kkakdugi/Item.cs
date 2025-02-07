348115a45b440df0a09d7020beeb434ea4c7212b
sangweon25 <seosangweon1290@gmail.com>
2025-02-07 15:43:13 +0900

    아이템,플레이어 클래스 생성
    아이템 클래스 ,플레이어 클래스 기초 정보 생성

---
+++ Kkakdugi/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kkakdugi
+{
+    enum AbilityType
+    {
+        Attack = 0,
+        Defense = 1,
+    }
+    
+    internal class Item
+    {
+        //아이템 세부 정보
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Ability { get; set; }
+        public  AbilityType Type{ get; set; }
+        public int Gold{ get; set; }
+        public bool IsEquip {  get; set; }
+
+
+        public Item(string name, string desc,int abil, AbilityType abilType,int gold )
+        {
+            Name = name;
+            Description = desc; 
+            Ability = abil;
+            Type = abilType;
+            Gold = gold;
+        }
+
+    }
+}
---
+++ Kkakdugi/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kkakdugi
+{
+    internal class Player
+    {
+        //플레이어 능력치, 세부정보
+        public string Name { get; set; }
+        public int Lv { get; set; }
+        public string Job { get; set; }
+        public int Hp { get; set; }
+        public int MaxHp { get; set; }
+        public int Atk { get; set; }
+        public int EquipAtk { get; set; }
+        public int Def { get; set; }
+        public int EquipDef { get; set; }
+        public int Gold { get; set; }
+
+        public Player(string name,string job, int lv, int maxHp, int atk, int gold)
+        {
+            Name = name;
+            Job = job;
+            Lv = lv;
+            Hp = maxHp;
+            Atk = atk;
+            Gold = gold;    
+        }
+
+        public void PrintPlayer()
+        {
+
+        }
+
+    }
+}
