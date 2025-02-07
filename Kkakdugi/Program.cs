b7adef5c009288a7812f991d8d93209b616f7add
sangweon25 <seosangweon1290@gmail.com>
2025-02-07 14:33:03 +0900

    Create SceneManager
    씬메니저, 생성

--- Kkakdugi/Program.cs
+++ Kkakdugi/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            SceneManager sceneManager = new SceneManager();
+
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Console.WriteLine("1./2.");
---
+++ Kkakdugi/SceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kkakdugi
+{
+    internal class SceneManager
+    {
+        List<StartBattle_.Monster> monsters;
+
+        public SceneManager()
+        {
+
+            monsters = new List<StartBattle_.Monster>
+            {
+                new StartBattle_.Monster("몬스터1",2,15,5),
+                new StartBattle_.Monster("몬스터2",3,10,9),
+                new StartBattle_.Monster("몬스터3",5,25,8),
+            };
+        }
+
+
+        //매개변수로 들어온 몬스터가 공격 
+        public void EnemyPhase(StartBattle_.Monster monster)
+        {
+            Console.WriteLine("Battle!!\n");
+            Console.WriteLine("Player을(를) 맞췄습니다. [데미지 :monsters.atk]");
+
+            //if 플레이어가 Dead상태가 아니라면 공격하지 않는다.
+            //플레이어 데미지 받는 함수는 플레이어 내부에 구현 예정
+            Console.WriteLine("Lv. {player.Level} {player.Name}");
+            Console.WriteLine("HP {player.Hp} -> player.Hp - monsters.atk\n");
+            Console.WriteLine("0. 다음\n");
+
+            Console.WriteLine("대상을 선택해주세요.");
+            Console.Write(">>");
+            Console.ReadLine();
+
+        }
+    }
+}
