484072412308af6b28a248aff2072cea0cc03174
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2025-02-10 10:49:28 +0900

    공격 결과 창 몬스터 Hp 출력 수정
    치명타 출력 미구현, SceneManager에 Attack 관련 메서드 추가

--- Kkakdugi/Attack_.cs
+++ Kkakdugi/Attack_.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace Kkakdugi
@@ -66,7 +67,7 @@ namespace Kkakdugi
             Console.WriteLine($"{player.Name} 의 공격!");
             Console.Write($"Lv.");
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write($"{monster.Lev}");
+            Console.Write($"{monster.Lev} ");
             Console.ResetColor();
             Console.Write($"{monster.Name}을(를) 맞췄습니다. [데미지 : ");
             Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -75,21 +76,28 @@ namespace Kkakdugi
             Console.Write("]");
 
             ////만약 치명타가 터졌다면
-            //if(isCritical == true)
+            //bool isCritical = FinalAtk > player.Atk * 1.6 - 1; 
+            //if (isCritical)
             //{
             //    Console.WriteLine(" - 치명타 공격 !!");
             //}
+
             Console.WriteLine();
-            Console.WriteLine($"Lv.{monster.Lev} {monster.Name}");
+            Console.WriteLine();
+            Console.Write($"Lv.");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"{monster.Lev} ");
+            Console.ResetColor();
+            Console.WriteLine($"{monster.Name}");
 
             // 몬스터 출력 여부 다르게 하기
             if (monster.Hp <= 0)
             {
-                Console.WriteLine($"HP {monster.Hp + FinalAtk} -> [Dead]");
+                Console.WriteLine($"HP {monster.Hp + (int)FinalAtk} -> [Dead]");
             }
             else
             {
-                Console.WriteLine($"HP {monster.Hp + FinalAtk} -> {monster.Hp}");
+                Console.WriteLine($"HP {monster.Hp + (int)FinalAtk} -> {monster.Hp}");
             }
 
             Console.WriteLine();
@@ -106,18 +114,6 @@ namespace Kkakdugi
             }
         }
 
-        
-    
-        /* 병합 할 때 해야 할 것들 정리
-          
-        만들어진 몬스터 가져오기, 랜덤 값 있으면 받아오기
-        반복문 이용해서 출력하기 (앞에 번호 생기게 하기)
-
-        플레이어 정보 가져오기. 
-        원하는 만큼 출력 시키기.
-
-
-        */
     }
 }
 
