9da3e5560a5fbd7d04e9de099017c430c72a80ed
sangweon25 <seosangweon1290@gmail.com>
2025-02-10 19:06:08 +0900

    Create Store
    SceneManager 클래스에 상점 출력을 위한 메서드 생성(진행중)

--- Kkakdugi/SceneManager.cs
+++ Kkakdugi/SceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,17 @@ namespace Kkakdugi
 {
     internal class SceneManager
     {
-        List<Monster> monsters;
-
-        public SceneManager()
+        Player player = new Player("김치", "음식", 1, 100, 15, 1000);
+        Attack_ attack = new Attack_();
+        List<Item> itemList = new List<Item>
         {
+            new Item(AbilityType.방어구,"무껍질",2,"무를 보호해주는 껍질이다.",500),
+            new Item(AbilityType.방어구,"나무판자",4,"나무 판자안에 무는 더욱 안전해보인다.",1000),
+            new Item(AbilityType.무기,"나뭇가지",2,"흔해빠진 나뭇가지다.",500),
+            new Item(AbilityType.무기,"돌칼",4,"약간은 날카로운 돌이다.",1000),
+        };
 
-            monsters = new List<Monster>
-            {
-                new Monster("몬스터1",2,15,5),
-                new Monster("몬스터2",3,10,9),
-                new Monster("몬스터3",5,25,8),
-            };
-        }
-
-        // 몬스터 정보 출력
+        // 몬스터 정보 출력    
         public void MonsterPrintInfo()
         {
             Console.WriteLine("Battle!!");
@@ -46,7 +44,20 @@ namespace Kkakdugi
             {
                 m.MonsterState(m);
             }
-        }
+
+            player.PrintPlayer();
+
+            Console.WriteLine();
+            Console.WriteLine("1. 공격");
+            Console.WriteLine();
+            if(InputManager.GetInput(1,1) == 1)
+            {
+                Console.Clear();
+                //현재 AttackInfo내에 player객체를 넘겨줘서 사용해야함.
+                attack.AttackInfo(randmonsters);
+            }
+        }//MonsterPrintInfo Method
+
         //매개변수로 들어온 몬스터가 공격 
         public void EnemyPhase(Monster monster,Player player)
         {
@@ -57,9 +68,33 @@ namespace Kkakdugi
             //플레이어 데미지 받는 함수는 플레이어 내부에 구현 예정
             Console.WriteLine($"Lv. {player.Lv} {player.Name}");
             Console.WriteLine($"HP {player.Hp} -> {player.RecieveDamage(monster.Atk)}\n");
-
+           
             InputManager.inputNext();
 
+        }//EnemyPhase Method
+
+        //===============================상점===============================
+
+        public void StoreScene()
+        {
+            Console.WriteLine("Store");
+
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine();
+            //Console.WriteLine("|분류\t|이름\t\t|능력치\t|설명\t\t\t|가격|");
+            SortingString();
+            Console.WriteLine("===================================================================");
+            for(int i =0; i < itemList.Count; i++)
+            {
+                itemList[i].PrintItems();
+            }
+
         }
-    }
-}
+
+        //아이템 상점에 출력될 문자열 간격을 맞춰주는 함수
+        public void SortingString()
+        {
+            Console.WriteLine("{0,-15}| {1,-15}| {2,-15}| {3,-15}| {4,-15}|", "분류","이름","능력치","설명","가격");
+        }
+    }//class SceneManager
+}//namespace
