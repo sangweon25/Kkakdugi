11a4ec29aa9d10a4e31d82224b73f4c75aa7322e
Suu <jsuu1997@gmail.com>
2025-02-10 19:41:56 +0900

    Suu_인벤토리 업데이트
    아이템 리스트 생성
    인벤토리 선택시 보여줄 아이템 목록 작성

--- Kkakdugi/Inventory_.cs
+++ Kkakdugi/Inventory_.cs
@@ -8,5 +8,28 @@ namespace Kkakdugi
 {
     internal class Inventory_
     {
+        private List<Item> item;
+        public Inventory_()
+        {
+            item = new List<Item>();
+        }
+
+        // 인벤토리 창 선택시 아이템 목록 출력
+        public void ItemprintInfo()
+        {
+            Console.WriteLine("[아이템 목록]\n");
+            if (item == null)
+            {
+                Console.WriteLine("인벤토리가 비어 있습니다.");
+            }
+            else
+            {
+                foreach ( Item items in item)
+                {
+                    Console.WriteLine(items);
+                }
+            }
+        }
+       
     }
 }
