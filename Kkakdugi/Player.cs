009fcb5c202d1c359c2c648c86dc45f63647e043
peachhike <peachhike@naver.com>
2025-02-10 20:19:24 +0900

    1-2hyun

--- Kkakdugi/Player.cs
+++ Kkakdugi/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,16 +21,33 @@ namespace Kkakdugi
         public int EquipDef { get; set; }
         public int Gold { get; set; }
 
-        public Player(string name,string job, int lv, int maxHp, int atk, int gold)
+        public Player(string name,string job, int lv, int maxHp, int atk, int gold, int def)
         {
             Name = name;
-            Job = job;
+            Job = "전사";
             Lv = lv;
             Hp = maxHp;
             Atk = atk;
-            Gold = gold;    
+            Gold = gold;
+            EquipAtk = 0;
+            Def = def;
         }
 
+
+        public void StatusDisplay()
+        {
+            Console.WriteLine($"Lv. {AuthenticationLevel.ToString("00")}");
+            Console.WriteLine($"{Name} ({Job}");
+            
+            string str = EquipAtk == 0 ? $"공격력 : {Atk}" : $"공격력: {Atk + EquipAtk} (+{EquipAtk})";
+            Console.WriteLine(str);
+            str=EquipDef == 0 ? $"방어력 : {Def}" : $"방어력 : {Def + EquipDef} (+{EquipDef})";
+            Console.WriteLine(str);
+            
+            Console.WriteLine($"체력: {Hp} / {MaxHp}");
+            Console.WriteLine($"Gold : {Gold} G");
+            
+        }
         //플레이어 필드 내용 출력 함수
         public void PrintPlayer()
         {
