078b79addd40b6ef4163ca51d00085de1197c5ad
sangweon25 <seosangweon1290@gmail.com>
2025-02-11 09:40:41 +0900

    ConneectStatusScreen
    메인씬에 상태보기 갈수있도록 연결부 추가 시작화면 -> 상태보기
    상태보기->시작화면 (연결만)

--- Kkakdugi/Player.cs
+++ Kkakdugi/Player.cs
@@ -36,7 +36,7 @@ namespace Kkakdugi
         public void StatusDisplay()
         {
             Console.WriteLine($"Lv. {Lv.ToString("00")}");
-            Console.WriteLine($"{Name} ({Job}");
+            Console.WriteLine($"{Name} ({Job})");
 
             string str = EquipAtk == 0 ? $"공격력 : {Atk}" : $"공격력: {Atk + EquipAtk} (+{EquipAtk})";
             Console.WriteLine(str);
