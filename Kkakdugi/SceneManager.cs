8b3eefcb011bcd1996a8bfca6c03c282b358f52e
Suu <jsuu1997@gmail.com>
2025-02-11 19:31:16 +0900

    장착기능 관리에서 메인으로 돌아가기 수정

--- Kkakdugi/SceneManager.cs
+++ Kkakdugi/SceneManager.cs
@@ -339,7 +339,7 @@ namespace Kkakdugi
             {
                 case 0:
                     Console.Clear();
-                    InventoryChoice();
+                    MainScene();
                     break;
             }
 
